c77f4ef1b6c215f74f453e6727e240400bc352b0
prumiha <overmcpro@gmail.com>
2025-09-10 00:29:14 +0200

    Can start app. Initial commit.

---
+++ src/test/java/com/teamupbe/TeamupBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamupbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamupBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
